9ae950c6b9b289c4a7017fbf8eacd342ff979e2f
AugustHK <Augustheltne@gmail.com>
2021-02-13 19:06:52 +0100

    Added author as a model field and implemented form for creating articles

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 # python manage.py makemigrations
 # python manage.py migrate
 
@@ -9,7 +10,7 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
     thumb = models.ImageField(default="default.png", blank=True)
-    # author
+    author = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
